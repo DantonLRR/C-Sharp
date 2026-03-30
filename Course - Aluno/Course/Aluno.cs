c38e8ae3490f29ea29f6fd1289574eaaa6609f8d
Danton Lucas <68911959+DantonLRR@users.noreply.github.com>
2026-03-30 16:27:52 -0300

    Add files via upload

---
+++ Course - Aluno/Course/Aluno.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course {
+    internal class Aluno {
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+
+        public string notaFinal() {
+            //media 60 pontos
+            double total = Nota1 + Nota2 + Nota3;
+            if (total > 60) {
+                return "Nota Final = " + total + " Aprovado";
+
+            }
+            else {
+                return "Nota Final = " + total + " Reprovado, faltaram: " + (60.00 - total) +" Pontos";
+            }
+        }
+    }
+}
---
+++ Course - Aluno/Course/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+namespace Course {
+
+    internal class Program {
+        static void Main(string[] args) {
+            Aluno aluno1 = new Aluno();
+            Console.WriteLine("Nome do aluno: ");
+      
+            aluno1.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno:");
+            aluno1.Nota1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno1.Nota2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno1.Nota3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(aluno1.notaFinal()); 
+
+        }
+
+
+    }
+}
